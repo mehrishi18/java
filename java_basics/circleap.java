e3d8b9c5b6923fe924ceea4ec60bf400a814a583
hrishi <sahrishikesh8@gmail.com>
2025-04-28 22:43:41 +0530

    adder java_basics modified

--- java_basics/circleap.java
+++ java_basics/circleap.java
@@ -3,7 +3,7 @@ package Java_basics;
  public class circleap{
     public static void main(String[] args) {
         double r=14,pi=3.14;
-
+//bvfdfghjvb
         System.out.println("area="+pi*r*r);
         System.out.println("perimeter"+2*pi*r);
     }
