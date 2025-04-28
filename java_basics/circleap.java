e95edc04e9f5f1d26e6962635be0650932bb74cc
hrishi <sahrishikesh8@gmail.com>
2025-04-28 22:34:43 +0530

    adder java_basics

---
+++ java_basics/circleap.java
@@ -0,0 +1,12 @@
+package Java_basics;
+ 
+ public class circleap{
+    public static void main(String[] args) {
+        double r=14,pi=3.14;
+
+        System.out.println("area="+pi*r*r);
+        System.out.println("perimeter"+2*pi*r);
+    }
+ 
+    
+ }
---
+++ java_basics/rectap.java
@@ -0,0 +1,9 @@
+package Java_basics;;
+public class rectap{
+    public static void main(String args[]){
+        int l=12,b=13;
+        System.out.println("area:"+l*b);
+        System.out.println("perimeter:"+2*(l+b));
+
+    }
+}
